2f1a6b472713927b2fd2aa090f01ccfaed764262
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-03-04 11:01:34 +0100

    feat: header, button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
