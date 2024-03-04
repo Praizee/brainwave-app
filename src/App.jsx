97c5181c90f9b917f3f296098a6f2b921d3cbb64
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-03-04 09:35:24 +0100

    chore: styles, fonts, page title, description

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
