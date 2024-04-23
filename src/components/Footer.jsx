dab8a7286654fea83836c3502352c57cb1eaf16e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-04-23 13:15:12 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,14 @@ const Footer = () => {
     <Section crosses className="!px-0 !py-10">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <p className="caption text-n-4 lg:block">
-          © {new Date().getFullYear()}. All rights reserved.
+          © {new Date().getFullYear()}.{" "}
+          <a
+            href="https://stephen-adeniji.vercel.app/"
+            className="transition-colors hover:text-color-1"
+          >
+            Stephen Adeniji
+          </a>{" "}
+          All rights reserved.
         </p>
 
         <ul className="flex gap-5 flex-wrap">
